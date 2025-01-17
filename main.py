e6477431bb002478ce7cc61c4cff9ac8a6052f02
Steven <91300742+Steven-Eon@users.noreply.github.com>
2025-01-17 10:15:51 -0800

    Helper functions implemented.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+def main():
+    filePath = "./books/frankenstein.txt"
+    try:
+        data = openFile(filePath)
+    except Exception as e:
+        print(e)
+
+
 def getWordCount(data):
+    if (type(data) != str):
+        raise Exception("String not provided.")
     wordSplit = data.split()
     return len(wordSplit)
 
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(getWordCount(file_contents))
+def getCharCount(data):
+    if (type(data) !=  str):
+        raise Exception("String not provided.")
+    charCount = {}
+    for i in data.split():
+        for j in range(len(i)):
+            if i[j].lower() in charCount:
+                charCount[i[j].lower()] = charCount[i[j].lower()] + 1
+            else:
+                charCount[i[j].lower()] = 1
+    return charCount
+
+def openFile(filePath):  
+    with open(filePath) as f:
+        return f.read()
+
